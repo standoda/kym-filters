5761188594ff58abb499cbd22d480a6eda6d84a7
standoda <47794473+standoda@users.noreply.github.com>
2019-02-23 22:48:23 +0100

    Update image_filters.user.js

--- image_filters.user.js
+++ image_filters.user.js
@@ -4,7 +4,6 @@
 // @description  Filter images in photo galleries based on entry/user
 // @author       e
 // @match        https://knowyourmeme.com/*photos*
-// @match        https://knowyourmeme.com/search/*
 // @match        https://knowyourmeme.com/memes/*
 // @match        https://knowyourmeme.com/users/*
 // @match        https://knowyourmeme.com/search*
